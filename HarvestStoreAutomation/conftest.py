dd7d2e6686c98fc9aeb3b420c7c5d4418b53a9f9
Yevheniia Silenko <prencezza12@gmail.com>
2023-11-17 23:06:45 +0200

    changes

--- HarvestStoreAutomation/conftest.py
+++ HarvestStoreAutomation/conftest.py
@@ -19,3 +19,23 @@ def account_page(login):
     account_page = AccountPage(login.driver)
     account_page.wait_for_account_page_to_load()
     return account_page
+
+import pytest
+from selenium import webdriver
+from HarvestStoreAutomation.page_objects.login_page import LoginPage
+
+@pytest.fixture
+def login(setup):
+    driver = setup
+    login_page = LoginPage(driver)
+    login_page.open("https://harvest-clothing.com.ua/login/")
+    login_page.login("prencezza12@gmail.com", "Canima_222")
+    return login_page
+
+import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+
+@pytest.fixture
+def wait(setup):
+    return WebDriverWait(setup, 20)
+
