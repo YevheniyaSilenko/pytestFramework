84636f07fdbd9ee79c47b4ff9eb7817b29ccda91
Yevheniia Silenko <prencezza12@gmail.com>
2023-11-20 02:00:05 +0200

    changes

--- HarvestStoreAutomation/page_objects/main_page.py
+++ HarvestStoreAutomation/page_objects/main_page.py
@@ -1,32 +1,70 @@
+import self
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-from HarvestStoreAutomation.page_objects.base_page import BasePage
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
-class MainPage(BasePage):
-    def __init__(self, driver):
-        super().__init__(driver)
+class MainPage:
+    WISHLIST_LOCATOR = (By.XPATH, '//*[@id="wishlist-total"]')  # Update this locator based on your HTML
+    ACCOUNT_LOCATOR = (By.CSS_SELECTOR, "i.icon.icon-person")  # Update this locator based on your HTML
+    CART_LOCATOR = (By.CSS_SELECTOR, "#cart-total > i")
+    INFORMATION_LOCATOR = (By.XPATH, '//a[text()="Інформація"]')
+ # Update this locator based on your HTML
+    CONTACTS_LOCATOR = (By.XPATH, '//*[@id="Контакти"]') # Update this locator based on your HTML
+
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+
+    def find_element(self, *locator):
+        return self.driver.find_element(*locator)
 
     def navigate_to_main_page(self):
-        self.open(self.config['page_urls']['main_page'])
+        self.driver.get("https://harvest-clothing.com.ua/")  # Update with your actual URL
+
+    def get_page_title(self):
+        return self.driver.title
+
+    def click_wishlist(self):
+        self.click_element(self.WISHLIST_LOCATOR)
+
+    def click_account(self):
+        self.click_element(self.ACCOUNT_LOCATOR)
+
+    def click_cart(self):
+        self.click_element(self.CART_LOCATOR)
+
+    def click_contacts(self):
+        self.click_element(self.CONTACTS_LOCATOR)
+
+    def click_information(self):
+        # Scroll to the "Інформація" element
+        information_element = self.find_element(*self.INFORMATION_LOCATOR)
+        self.scroll_into_view(information_element)
+        self.click_element(self.INFORMATION_LOCATOR)
+
+    def scroll_to_footer(self):
+        # Scroll to the bottom of the page
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    def click_information_in_footer(self):
+        # Scroll to the footer
+        self.scroll_to_footer()
+
+
+    def scroll_into_view(self, element):
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+
+    def scroll_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView({behavior: 'auto', block: 'center', inline: 'center'});", element)
+
+    def click_element(self, locator):
+        WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable(locator)).click()
 
     def get_title(self):
         return self.driver.title
 
-    def click_wishlist(self):
-        wishlist_locator = (By.XPATH, '//*[@id="wishlist-id"]')
-        self.click_element(wishlist_locator)
 
-    def click_account(self):
-        account_locator = (By.XPATH, '//*[@id="account-id"]')
-        self.click_element(account_locator)
-
-    def click_cart(self):
-        cart_locator = (By.XPATH, '//*[@id="cart-id"]')
-        self.click_element(cart_locator)
-
-    def click_call(self):
-        call_locator = (By.XPATH, '//*[@id="call-id"]')
-        self.click_element(call_locator)
-
-    def click_menu(self):
-        menu_locator = (By.XPATH, '//*[@id="menu-id"]')
-        self.click_element(menu_locator)
--- HarvestStoreAutomation/tests/test_main_page.py
+++ HarvestStoreAutomation/tests/test_main_page.py
@@ -1,40 +1,57 @@
 import pytest
+from selenium import webdriver
 from HarvestStoreAutomation.page_objects.main_page import MainPage
 
-@pytest.mark.usefixtures("setup")
 class TestMainPage:
+    @pytest.fixture
+    def setup(self):
+        # Set up the WebDriver
+        driver = webdriver.Chrome()
+        yield driver
+        # Teardown - close the WebDriver
+        driver.quit()
 
     def test_main_page_title(self, setup):
         main_page = MainPage(setup)
         main_page.navigate_to_main_page()
-        assert "Your Main Page Title" in main_page.get_title()
+        assert "Harvest - рюкзаки та сумки від українського молодіжного бренду" in main_page.get_title()
 
     def test_click_wishlist(self, setup):
         main_page = MainPage(setup)
         main_page.navigate_to_main_page()
         main_page.click_wishlist()
-        # Додайте перевірку для переходу на сторінку бажань
+        assert "Мої закладки" in main_page.get_title()
 
     def test_click_account(self, setup):
         main_page = MainPage(setup)
         main_page.navigate_to_main_page()
         main_page.click_account()
-        # Додайте перевірку для переходу на сторінку облікового запису
+
+
 
     def test_click_cart(self, setup):
         main_page = MainPage(setup)
         main_page.navigate_to_main_page()
         main_page.click_cart()
-        # Додайте перевірку для переходу на сторінку кошика
 
-    def test_click_call(self, setup):
+
+    def test_click_contacts(self, setup):
         main_page = MainPage(setup)
         main_page.navigate_to_main_page()
-        main_page.click_call()
-        # Додайте перевірку для виклику
+        main_page.click_contacts()
+        assert "Контакти" in main_page.get_title()
 
-    def test_click_menu(self, setup):
+
+
+    def test_click_infirmation(self, setup):
         main_page = MainPage(setup)
         main_page.navigate_to_main_page()
-        main_page.click_menu()
-        # Додайте перевірку для відкриття меню
+        main_page.click_information()
+
+
+
+if __name__ == "__main__":
+    pytest.main()
+
+
+
