071f6e7d1f6a6e4feb5142812f75f5d956ea50c2
Yevheniia Silenko <prencezza12@gmail.com>
2023-11-20 15:43:33 +0200

    changes

--- HarvestStoreAutomation/page_objects/new_arrivals_page.py
+++ HarvestStoreAutomation/page_objects/new_arrivals_page.py
@@ -1,7 +1,65 @@
-from base_page import BasePage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-class MenApparelPage(BasePage):
-    def __init__(self, driver):
-        super().__init__(driver)
-        self.url = 'https://store.ferrari.com/en-us/men/apparel'
-    # Додайте елементи та методи для сторінки чоловічого одягу за необхідності
+
+
+class NewArrivalsPage:
+    # Define locators for 10 clickable elements
+    SWEETSHOT1_LOCATOR = (By.ID,'<a href="https://harvest-clothing.com.ua/clothing/zhenschinam/w-hud%D1%96-ta-sv%D1%96tshoty/sweatshirt-holliday-black" id="hollidayLink">Світшот "HOLLIDAY" з блискавкою чорний</a>')
+    ELEMENT2_LOCATOR = (By.XPATH, '//*[@id="element2"]')
+    ELEMENT3_LOCATOR = (By.XPATH, '//*[@id="element3"]')
+    ELEMENT4_LOCATOR = (By.XPATH, '//*[@id="element4"]')
+    ELEMENT5_LOCATOR = (By.XPATH, '//*[@id="element5"]')
+    ELEMENT6_LOCATOR = (By.XPATH, '//*[@id="element6"]')
+    ELEMENT7_LOCATOR = (By.XPATH, '//*[@id="element7"]')
+    ELEMENT8_LOCATOR = (By.XPATH, '//*[@id="element8"]')
+    ELEMENT9_LOCATOR = (By.XPATH, '//*[@id="element9"]')
+    ELEMENT10_LOCATOR = (By.XPATH, '//*[@id="element10"]')
+
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+        def find_element(self, *locator):
+            return self.driver.find_element(*locator)
+
+        def navigate_to_main_page(self):
+            self.driver.get("https://harvest-clothing.com.ua/new")
+
+        def get_page_title(self):
+            return self.driver.title
+
+    def click_element1(self):
+        self.click_element(self.SWEETSHOT1_LOCATOR)
+
+    def click_element2(self):
+        self.click_element(self.ELEMENT2_LOCATOR)
+
+    def click_element3(self):
+        self.click_element(self.ELEMENT3_LOCATOR)
+
+    def click_element4(self):
+        self.click_element(self.ELEMENT4_LOCATOR)
+
+    def click_element5(self):
+        self.click_element(self.ELEMENT5_LOCATOR)
+
+    def click_element6(self):
+        self.click_element(self.ELEMENT6_LOCATOR)
+
+    def click_element7(self):
+        self.click_element(self.ELEMENT7_LOCATOR)
+
+    def click_element8(self):
+        self.click_element(self.ELEMENT8_LOCATOR)
+
+    def click_element9(self):
+        self.click_element(self.ELEMENT9_LOCATOR)
+
+    def click_element10(self):
+        self.click_element(self.ELEMENT10_LOCATOR)
+
+    def click_element(self, locator):
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator)).click()
+
---
+++ HarvestStoreAutomation/tests/test_new_arrivals_page.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from HarvestStoreAutomation.page_objects.new_arrivals_page import NewArrivalsPage
+
+class TestNewArrivalsPage:
+    @pytest.fixture
+    def setup(self):
+        # Set up the WebDriver
+        driver = webdriver.Chrome()
+        yield driver
+        # Teardown - close the WebDriver
+        driver.quit()
+
+    def test_new_arrivals_page_title(self, setup):
+        new_arrivals_page = NewArrivalsPage(setup)
+        new_arrivals_page.navigate_to_new_arrivals_page()
+        assert "Новинки українського бренду HARVEST" in new_arrivals_page.get_title()
+
+    def test_click_sweetshot1(self, setup):
+        new_arrivals_page = NewArrivalsPage(setup)
+        new_arrivals_page.navigate_to_new_arrivals_page()
+        new_arrivals_page.click_sweetshot1()
+        # Add assertions or verifications as needed
+        assert "Expected Result" in new_arrivals_page.get_title()
+
+    def test_click_element2(self, setup):
+        new_arrivals_page = NewArrivalsPage(setup)
+        new_arrivals_page.navigate_to_new_arrivals_page()
+        new_arrivals_page.click_element2()
+        # Add assertions or verifications as needed
+        assert "Expected Result" in new_arrivals_page.get_title()
+
+    # ... Repeat similar tests for other elements (element3, element4, ..., element10)
