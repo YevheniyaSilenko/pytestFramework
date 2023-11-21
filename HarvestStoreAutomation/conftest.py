62bfcb0e6a3cf7b98f5b18adcb2b8d3e09b47a76
Yevheniia Silenko <prencezza12@gmail.com>
2023-11-21 19:31:19 +0200

    changes

--- HarvestStoreAutomation/conftest.py
+++ HarvestStoreAutomation/conftest.py
@@ -1,9 +1,14 @@
 import pytest
 from selenium import webdriver
+from HarvestStoreAutomation.page_objects.login_page import LoginPage
+from HarvestStoreAutomation.page_objects.account_page import AccountPage
+
 
 @pytest.fixture
-def setup(request):
+def setup():
     driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
 
 
     def teardown():
@@ -19,9 +24,6 @@ def account_page(login):
     account_page.wait_for_account_page_to_load()
     return account_page
 
-import pytest
-from selenium import webdriver
-from HarvestStoreAutomation.page_objects.login_page import LoginPage
 
 @pytest.fixture
 def login(setup):
@@ -31,19 +33,31 @@ def login(setup):
     login_page.login("prencezza12@gmail.com", "Canima_222")
     return login_page
 
-import pytest
-from selenium.webdriver.support.ui import WebDriverWait
-
 @pytest.fixture
 def wait(setup):
     return WebDriverWait(setup, 20)
 
-def setup():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
 
 def actions(setup):
         driver = setup
         login_page = LoginPage(driver)
+
+
+def browser():
+    # This fixture provides a Selenium WebDriver instance
+    driver = webdriver.Chrome()  # You can use other browser drivers as well
+    yield driver
+    driver.quit()  # This will be executed after the test to clean up resources
+
+
+@pytest.fixture
+def login_page(browser):
+    # This fixture provides a LoginPage instance
+    return LoginPage(browser)
+
+
+@pytest.fixture
+def account_page(browser):
+    # This fixture provides an AccountPage instance
+    return AccountPage(browser)
 
