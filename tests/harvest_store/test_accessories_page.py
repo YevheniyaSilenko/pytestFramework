29c16a0681fb082bc70544ff1d9e0e71848e95dc
Yevheniia Silenko <prencezza12@gmail.com>
2023-11-26 11:37:05 +0200

    change

--- tests/harvest_store/test_accessories_page.py
+++ tests/harvest_store/test_accessories_page.py
@@ -1,61 +1,55 @@
+# Smoke Test: Verifies the behavior of clicking on the 'BACKPACKS' option
 def test_click_for_backpacks(open_accessories_page):
     accessories_page = open_accessories_page
     accessories_page.click_for_backpacks()
-    # Add assertions or verifications as needed
-    excepted_title = 'Купити рюкзак молодіжний в Україні - інтернет магазин Harvest'
-    assert excepted_title == accessories_page.get_title()
+    expected_title = 'Купити рюкзак молодіжний в Україні - інтернет магазин Harvest'
+    assert expected_title == accessories_page.get_title()
 
-
+# Smoke Test: Verifies the behavior of clicking on the 'BAGS' option
 def test_click_for_bags(open_accessories_page):
     accessories_page = open_accessories_page
     accessories_page.click_for_bags()
-    # Add assertions or verifications as needed
-    excepted_title = 'Купити сумки українського виробника Harvest шкіряні та з еко-шкіри'
-    assert excepted_title == accessories_page.get_title()
+    expected_title = 'Купити сумки українського виробника Harvest шкіряні та з еко-шкіри'
+    assert expected_title == accessories_page.get_title()
 
+# Smoke Test: Verifies the behavior of clicking on the 'OTHER' option
 def test_click_for_other(open_accessories_page):
     accessories_page = open_accessories_page
     accessories_page.click_for_other()
-    # Add assertions or verifications as needed
-    excepted_title = 'Портмоне та гаманці від українського виробника Harvest'
-    assert excepted_title == accessories_page.get_title()
+    expected_title = 'Портмоне та гаманці від українського виробника Harvest'
+    assert expected_title == accessories_page.get_title()
 
+# Smoke Test: Verifies the behavior of clicking on the 'COLLECTIONS' option
 def test_click_for_collections(open_accessories_page):
     accessories_page = open_accessories_page
     accessories_page.click_for_collections()
-    # Add assertions or verifications as needed
-    excepted_title = 'Колекції від українського виробника | HARVEST'
-    assert excepted_title == accessories_page.get_title()
+    expected_title = 'Колекції від українського виробника | HARVEST'
+    assert expected_title == accessories_page.get_title()
 
+# Regression Test: Verifies the behavior of clicking on the 'VINTAGE' option
 def test_click_for_vintage(open_accessories_page):
     accessories_page = open_accessories_page
     accessories_page.click_for_vintage()
-    # Add assertions or verifications as needed
-    excepted_title = 'Колекція аксесуарів VINTAGE від Harvest'
-    assert excepted_title == accessories_page.get_title()
+    expected_title = 'Колекція аксесуарів VINTAGE від Harvest'
+    assert expected_title == accessories_page.get_title()
 
-
+# Regression Test: Verifies the behavior of clicking on the 'ACTIVE' option
 def test_click_for_active(open_accessories_page):
     accessories_page = open_accessories_page
     accessories_page.click_for_active()
-    # Add assertions or verifications as needed
-    excepted_title = 'Колекція аксесуарів ACTIVE від Harvest'
-    assert excepted_title == accessories_page.get_title()
+    expected_title = 'Колекція аксесуарів ACTIVE від Harvest'
+    assert expected_title == accessories_page.get_title()
 
-
+# Regression Test: Verifies the behavior of clicking on the 'SHUTTLE' option
 def test_click_for_shuttle(open_accessories_page):
     accessories_page = open_accessories_page
     accessories_page.click_for_shuttle()
-    # Add assertions or verifications as needed
-    excepted_title = 'Колекція аксесуарів SHUTTLE від Harvest'
-    assert excepted_title == accessories_page.get_title()
+    expected_title = 'Колекція аксесуарів SHUTTLE від Harvest'
+    assert expected_title == accessories_page.get_title()
 
-
+# Regression Test: Verifies the behavior of clicking on the 'CLASSIC' option
 def test_click_for_classic(open_accessories_page):
     accessories_page = open_accessories_page
     accessories_page.click_for_classic()
-    # Add assertions or verifications as needed
-    excepted_title = 'Колекція аксесуарів CLASSIC від Harvest'
-    assert excepted_title == accessories_page.get_title()
-
-
+    expected_title = 'Колекція аксесуарів CLASSIC від Harvest'
+    assert expected_title == accessories_page.get_title()
--- tests/harvest_store/test_account_page.py
+++ tests/harvest_store/test_account_page.py
@@ -1,28 +1,26 @@
+# Regression Test: Verifies the behavior of editing the account
 def test_edit_account(open_account_page):
     account_page = open_account_page
     # Verify that we are on the account page
     assert "https://harvest-clothing.com.ua/account/" in account_page.get_current_url()
     account_page.click_edit_account()
     assert "https://harvest-clothing.com.ua/edit-account/" in account_page.get_current_url()
-    # Perform actions specific to the edit account page if needed
 
-
+# Regression Test: Verifies the behavior of changing the password
 def test_change_password(open_account_page):
     account_page = open_account_page
     # Change Password
     account_page.click_change_password()
     assert "https://harvest-clothing.com.ua/change-password/" in account_page.get_current_url()
-    # Perform actions specific to the change password page if needed
 
-
+# Regression Test: Verifies the behavior of changing the address
 def test_change_address(open_account_page):
     account_page = open_account_page
     # Change Address
     account_page.click_change_address()
     assert "https://harvest-clothing.com.ua/address-book/" in account_page.get_current_url()
-    # Perform actions specific to the change address page if needed
 
-
+# Regression Test: Verifies the behavior of accessing the orders
 def test_orders(open_account_page):
     account_page = open_account_page
     # Orders
