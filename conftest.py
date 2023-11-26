c353085bd78e7c640168273fbedfb9ad1328bc70
Yevheniia Silenko <prencezza12@gmail.com>
2023-11-26 23:04:20 +0200

    change

--- conftest.py
+++ conftest.py
@@ -9,7 +9,6 @@ import utilities.config_reader
 import allure
 
 
-
 @pytest.fixture
 def create_driver():
     driver = DriverFactory(
@@ -52,6 +51,18 @@ def open_accessories_page(login):
     login.driver.get(utilities.config_reader.AppConfig.accessories_page_url)
     return AccessoriesPage(login.driver)
 
-def pytest_html_report_title(report):
-    report.title = "Hillel test report"
 
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+    setattr(item, "rep_" + rep.when, rep)
+
+def pytest_addoption(parser):
+    parser.addoption('--env', action='store', default='dev', help='Choose your env')
+    parser.addoption('--hub', action='store', default='False', help='Run test in container Selenoid')
+    parser.addoption('--headless', action='store', default='False', help='Run test in headless mode')
+    parser.addoption('--browser', action='store', default='1', help='Choose yor browser (1- chrome, 2 -firefox)')
