238fcb4b3079f33f0d7e3552cee23e20dc482ee3
Yevheniia Silenko <prencezza12@gmail.com>
2023-12-08 17:37:58 +0200

    changes

--- conftest.py
+++ conftest.py
@@ -1,18 +1,16 @@
 import json
 
 import pytest
+
 import constants
-from constants import ROOT_PATH
+import utilities.config_reader
+import utilities.config_reader
 from db.sqlite_pack.products_repo import ProductsRepo
 from page_objects.accessories_page import AccessoriesPage
-from page_objects.login_page import LoginPage
 from page_objects.account_page import AccountPage
-from utilities.driver_factory import DriverFactory
-import utilities.config_reader
 from page_objects.clothing_page import ClothingPage
-import utilities.config_reader
-import allure
-
+from page_objects.login_page import LoginPage
+from utilities.driver_factory import DriverFactory
 from utilities.json_to_dict import DictToClass
 
 
