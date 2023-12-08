e6cf41c143dfbc7d8bd86c2a5dc8a3620cefc5af
Yevheniia Silenko <prencezza12@gmail.com>
2023-12-08 14:30:55 +0200

    ping site

---
+++ subprocess_module/ping_site.py
@@ -0,0 +1,30 @@
+import subprocess
+import platform
+
+
+def ping_websites(websites, count=2):
+    system_platform = platform.system().lower()
+
+    for website in websites:
+        print(f"Pinging {website}...")
+
+        command = ["ping", website]
+
+        if system_platform == "windows":
+            command += ["-n", str(count)]
+        elif system_platform == "linux" or system_platform == "darwin":
+            command += ["-c", str(count)]
+        else:
+            print(f"Unsupported operating system: {system_platform}")
+            return
+
+        try:
+            subprocess.run(command, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error while pinging {website}: {e}")
+
+        print("\n" + "=" * 30 + "\n")  # Separator
+
+
+websites_to_ping = ["www.google.com", "www.google.com"]
+ping_websites(websites_to_ping)
