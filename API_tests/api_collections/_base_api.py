11781e5574437cb6b505a718b3f1ec94677acfdb
Yevheniia Silenko <prencezza12@gmail.com>
2023-12-12 15:27:13 +0200

    changes

---
+++ API_tests/api_collections/_base_api.py
@@ -0,0 +1,34 @@
+import requests
+from jsonschema import validate
+
+
+class BaseApi:
+    def __init__(self, base_url):
+        self.__base_url = base_url
+        self.__headers = {}
+        self.__requests = requests
+
+    @staticmethod
+    def _validate_json(instance, schema):
+        validate(instance, schema)
+
+    def _get(self, url, headers=None):
+        if not headers:
+            headers = self.__headers
+        response = self.__requests.get(f'{self.__base_url}{url}', headers=headers)
+        return response
+
+    def _post(self, url, data, headers=None):
+        if not headers:
+            headers = self.__headers
+        response = self.__requests.post(f'{self.__base_url}{url}', data=data, headers=headers)
+        return response
+
+    def _patch(self, url, data, headers=None):
+        pass
+
+    def _put(self, url, data, headers=None):
+        pass
+
+    def _delete(self, url, data=None, headers=None):
+        pass
